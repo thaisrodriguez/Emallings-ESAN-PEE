f98c5640531798fd37b7a16e4085835a069ae018
Thais Rodriguez <thais.rodriguez23@outlook.com>
2017-08-31 12:37:48 -0500

    commit inicial emalling agosto listos pero sin los cambios de imagen

---
+++ header.php
@@ -0,0 +1,44 @@
+			<div class="Content desktop">
+				<div class="wrapper Cabecera">
+					<header class="header">
+						<div class="nav">
+							<a href="javascript:void(0)">
+								<img src="/img/phone-02.png" alt="">
+							</a>
+						</div>
+						<div class="logo">
+							<a href="/">
+								<img src="/img/logo-etna.png" alt="ETNA Express">
+							</a>
+						</div>
+						<div class="call movil">
+							<a href="tel:01 514 2535" id="phone">
+								<img src="/img/whatsapp.png" alt="514-2535">
+							</a>
+						</div>
+					</header>
+				</div>
+			</div>
+			<div class="Content mobile">
+				<div class="wrapper Cabecera">
+					<header class="header">
+						<div class="logo">
+							<a href="/">
+								<img src="/img/logo-etna.png" alt="ETNA Express">
+							</a>
+						</div>
+						<div>
+							<div class="nav">
+								<a href="javascript:void(0)">
+									<img src="/img/phone-02.png" alt="">
+								</a>
+							</div>
+							<div class="call movil">
+								<a href="tel:01 514 2535">
+									<img src="/img/whatsapp.png" alt="514-2535">
+								</a>
+							</div>
+						</div>
+					</header>
+				</div>
+			</div>
